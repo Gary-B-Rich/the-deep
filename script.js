34181300abe37c4e19738eecf353865f2124e23c
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-02-12 10:18:37 -0500

    start adding button func

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //  script.js for the-deep directory
+
+const button1=document.getElementById('button1');
+button1.addEventListener("click", function() {
+    console.log("Clicked button 1");
+});
+
+const button2=document.getElementById('button2');
+button2.addEventListener("click", function() {
+    console.log("Clicked button 2");
+});
+
+const button3=document.getElementById('button3');
+button3.addEventListener("click", function() {
+    console.log("Clicked button 3");
+});
+
+const button4=document.getElementById('button4');
+button4.addEventListener("click", function() {
+    console.log("Clicked button 4");
+});
